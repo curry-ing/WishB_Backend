1dcddd050469606fd30dec89490af96c25feee85
Ma Sunghoon <masunghoon@gmail.com>
2014-08-15 19:40:20 +0900

    Bug fixes

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -13,46 +13,44 @@ $(document).ready(function(){
     $("#logoutBtn").click(LogoutUser);
 });
 
-//var CheckAuthentication = function(){
-//    var auth;
-//    alert("2222");
-//    if (localStorage.getItem('token')){
-//        var auth_token = localStorage.getItem('token');
-//        var hash = $.base64.encode(auth_token + ':unused');
-//        $.ajax({
-//            url: '/api/resource',
-//            type: 'GET',
-//            beforeSend: function(xhr){
-//                xhr.setRequestHeader("Authorization", "Basic "+hash);
-//            },
-//            success: function(data){
-//                localStorage.removeItem('id','email','username');
-//                localStorage.setItem('id',data.data.id);
-//                localStorage.setItem('username',data.data.username);
-//                localStorage.setItem('email',data.data.email);
-//                alert(data.data.is_admin);
-//                if(data.data.is_admin == 0){
-//                    $("#menuInquiryLink").attr('href','/inquiry/write');
-//                } else {
-//                    $("#menuInquiryLink").attr('href','/inquiry');
-//                }
-//                auth = true;
-//            },
-//            error: function(jqXHR){
-//                console.log("ajax error " + jqXHR.status + ": " + jqXHR.description);
-//                auth = false;
-//            }
-//        });
-//    } else {
-//        auth = false;
-//    }
-//    return auth;
-//};
-//
-//var LogoutUser = function(){
-//    if(CheckAuthentication()){
-//        localStorage.clear();
-//        alert("Bye!");
-//    }
-//    window.location = '/index';
-//}
+var CheckAuthentication = function(){
+    var auth;
+    if (localStorage.getItem('token')){
+        var auth_token = localStorage.getItem('token');
+        var hash = $.base64.encode(auth_token + ':unused');
+        $.ajax({
+            url: '/api/resource',
+            type: 'GET',
+            beforeSend: function(xhr){
+                xhr.setRequestHeader("Authorization", "Basic "+hash);
+            },
+            success: function(data){
+                localStorage.removeItem('id','email','username');
+                localStorage.setItem('id',data.data.id);
+                localStorage.setItem('username',data.data.username);
+                localStorage.setItem('email',data.data.email);
+                if(data.data.is_admin == 0){
+                    $("#menuInquiryLink").attr('href','/inquiry/write');
+                } else {
+                    $("#menuInquiryLink").attr('href','/inquiry');
+                }
+                auth = true;
+            },
+            error: function(jqXHR){
+                console.log("ajax error " + jqXHR.status + ": " + jqXHR.description);
+                auth = false;
+            }
+        });
+    } else {
+        auth = false;
+    }
+    return auth;
+};
+
+var LogoutUser = function(){
+    if(CheckAuthentication()){
+        localStorage.clear();
+        alert("Bye!");
+    }
+    window.location = '/index';
+}
